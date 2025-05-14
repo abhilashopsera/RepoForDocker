17802228107bf6b351695afa2ebe4ede8c4e1b17
abhilashopsera <147802490+abhilashopsera@users.noreply.github.com>
2025-05-14 18:54:55 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,11 @@
-
+provider "aws" {
+  region = var.aws_region
+}
+resource "aws_s3_bucket" "docker_repo_bucket" {
+  bucket = var.bucket_name
+  acl    = "private"
+  tags = {
+    Name        = "docker-repo-s3-bucket"
+    Environment = "dev"
+  }
+}
